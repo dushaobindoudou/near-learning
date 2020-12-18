91b3de74926c72b9220e8b8044d6ce99763c168e
dushaobin <dushaobin@liepin.com>
2020-12-18 13:27:48 +0800

    feat: create wallet

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 
 
 const connect = require('./src/connect');
+const creactAccount = require('./src/create_account');
 
-connect.main();
+// 连接到datahub
+// connect.main();
+
+// 创建一个账号
+creactAccount.createAccount();
+
+
