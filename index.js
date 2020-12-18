bc719e74d5a2446a1f837b20642f199911288d1f
dushaobin <dushaobin@liepin.com>
2020-12-18 13:39:42 +0800

    feat: 完成链上状态获取

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 
 const connect = require('./src/connect');
 const creactAccount = require('./src/create_account');
+const node = require('./src/query');
 
 // 连接到datahub
 // connect.main();
 
 // 创建一个账号
-creactAccount.createAccount();
+// creactAccount.createAccount();
 
+node.query();
 
